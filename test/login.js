6ae1f5c270a3b0a643173230c491de407e11bff2
Marián Kapišinský <majokapisinsky@gmail.com>
2020-05-08 17:02:10 +0200

    Update login.js

--- test/login.js
+++ test/login.js
@@ -10,58 +10,73 @@ const msgStyle = {
 
 var user, tm, err;
 
-var ws = new WebSocket('ws://localhost:1234');
+if (!document.cookie.includes('SID')) {
 
-ws.onopen = function (e) {
+	var ws = new WebSocket('ws://localhost:1234');
+
+	ws.onerror = function (e) {
+		$("#status").text('WebSocket Error');
+	}
+
+	ws.onopen = function (e) {
 		$("#promptLabel").text('Username:');
 		$("#promptForm").show();
-};
+	};
 
-ws.onmessage = function(e) {
-	var message = JSON.parse(e.data);
+	ws.onmessage = function(e) {
+		var message = JSON.parse(e.data);
 
-	if (message.msg === PAM_SUCCESS) {
-		ws.close();
-		$("#promptForm").hide();
-		$("#status").text('Authenticated');
-		document.cookie = message.cookie;
-		window.location.href = '/app';
-	} else if (typeof message.msg === 'string') {
-			
-		switch (message.msgStyle) {
-			case msgStyle.PAM_PROMPT_ECHO_OFF:
-				$("#promptLabel").text(message.msg);
-				$('#prompt').prop('type', 'password');
-				startTimer();
-				break;
-			case msgStyle.PAM_PROMPT_ECHO_ON:
-				$("#promptLabel").text(message.msg);
-				$('#prompt').prop('type', 'text');
-				startTimer();
-				break;
-			case msgStyle.PAM_ERROR_MSG:
-			case msgStyle.PAM_TEXT_INFO:
-						
-				if (message.msgStyle === msgStyle.PAM_ERROR_MSG) {
-					$("#messages").append('<p style="color:red">' + message.msg + '</p>');
-				} else {
-					$("#messages").append('<p>' + message.msg + '</p>');
-				}
-				break;
-			default:
-				break;
+		if (message.msg === PAM_SUCCESS) {
+			ws.close();
+			$("#promptForm").hide();
+			$("#status").text('Authenticated');
+			document.cookie = message.cookie;
+			setTimeout( () => {
+				window.location.href = '/app';
+			}, 3000);
+		} else if (typeof message.msg === 'string') {
+				
+			switch (message.msgStyle) {
+				case msgStyle.PAM_PROMPT_ECHO_OFF:
+					$("#promptLabel").text(message.msg);
+					$('#prompt').prop('type', 'password');
+					startTimer();
+					break;
+				case msgStyle.PAM_PROMPT_ECHO_ON:
+					$("#promptLabel").text(message.msg);
+					$('#prompt').prop('type', 'text');
+					startTimer();
+					break;
+				case msgStyle.PAM_ERROR_MSG:
+				case msgStyle.PAM_TEXT_INFO:
+							
+					if (message.msgStyle === msgStyle.PAM_ERROR_MSG) {
+						$("#messages").append('<p style="color:red">' + message.msg + '</p>');
+					} else {
+						$("#messages").append('<p>' + message.msg + '</p>');
+					}
+					break;
+				default:
+					break;
+			}
+		} else {
+			clearTimeout(tm);
+			err = true;
+			user = undefined;
+			$("#status").text('Wrong username or password, please try again');
+			$('#prompt').prop('type', 'text');
+			$("#promptLabel").text('Username:');
 		}
-	} else {
-		clearTimeout(tm);
-		err = true;
-		user = undefined;
-		$("#status").text('Wrong username or password, please try again');
-		$('#prompt').prop('type', 'text');
-		$("#promptLabel").text('Username:');
-	}
-	$('#prompt').val('');
+		$('#prompt').val('');
+	};
+} else {
+	$(document).ready( () => {
+		$("#status").text('Already logged in');
+		setTimeout( () => {
+			window.location.href = '/app';
+		}, 3000);
+	});
 };
-
 
 function startTimer() {
 
@@ -89,5 +104,3 @@ function sendUserInput() {
 		ws.send(userInput);
 	}
 };
-
-
